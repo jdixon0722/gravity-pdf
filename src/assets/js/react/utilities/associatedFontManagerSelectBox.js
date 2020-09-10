d011a0cf0056f398fcbe6f5cdf4d15e285b2e7a8
Debbie <debbieellis7@gmail.com>
2020-09-10 12:22:31 +0800

    Ensure associated font manager select box has the latest data

--- src/assets/js/react/utilities/associatedFontManagerSelectBox.js
+++ src/assets/js/react/utilities/associatedFontManagerSelectBox.js
@@ -1,7 +1,11 @@
 export default (fontList) => {
-  const defaultFontSelectBox = document.querySelector('.gfpdf_settings_default_font ')
+  const defaultFontSelectBox = document.querySelector('.gfpdf_settings_default_font')
   const definedFontsOptgroup = document.querySelector('optgroup[label="User-Defined Fonts"]')
   const selectedValue = defaultFontSelectBox.options[defaultFontSelectBox.selectedIndex].value
+  const userDefinedFonts = []
+
+  /* Get current User-Defined Fonts items */
+  Array.from(document.querySelectorAll('optgroup[label="User-Defined Fonts"] > option')).map(item => userDefinedFonts.push(item.value))
 
   if (definedFontsOptgroup !== null) {
     /* Remove optgroup */
@@ -19,6 +23,34 @@ export default (fontList) => {
     optgroup.appendChild(option)
   })
 
+  const list = []
+
+  if (fontList.length > 0) {
+    fontList.map(font => list.push(font.shortname))
+  }
+
+  let updateSelectBoxValue
+
+  /* Assign default value if selected item is deleted */
+  if (userDefinedFonts.includes(selectedValue) && list.length !== 0 && !list.includes(selectedValue)) {
+    defaultFontSelectBox.insertBefore(optgroup, defaultFontSelectBox.childNodes[0])
+    updateSelectBoxValue = defaultFontSelectBox.selectedIndex = '0'
+
+    return updateSelectBoxValue
+  }
+
+  /* Perform deletion for the very last item left */
+  if (list.length === 0 && userDefinedFonts.length > 0) {
+    updateSelectBoxValue = defaultFontSelectBox.selectedIndex = '0'
+
+    return updateSelectBoxValue
+  }
+
   defaultFontSelectBox.insertBefore(optgroup, defaultFontSelectBox.childNodes[0])
   defaultFontSelectBox.value = selectedValue
+
+  /* Remove User-Defined Fonts field if empty */
+  if (userDefinedFonts.length === 0 && list.length === 0) {
+    defaultFontSelectBox.querySelector('optgroup[label="User-Defined Fonts"]').remove()
+  }
 }
